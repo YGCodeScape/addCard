a1091d7aeb67ef36a4c8bf4112f8155c3092998d
yashG <yrg.creation.ai@gmail.com>
2025-10-27 21:01:20 +0530

    form prevent

---
+++ script.js
@@ -0,0 +1,6 @@
+let form = document.querySelector("form");
+let input = document.querySelectorAll("input[type='text']");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+})
