e254592d1d5fc5a0b7c4d92e4273e47af3684e1c
yashG <yrg.creation.ai@gmail.com>
2025-10-27 21:19:00 +0530

    js DOM

--- script.js
+++ script.js
@@ -3,4 +3,31 @@ let input = document.querySelectorAll("input[type='text']");
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
+
+    let cardHolder = document.createElement("div");
+    cardHolder.classList.add("card-holder");
+    
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let profile = document.createElement("div");
+    profile.classList.add("profile");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", input[0].value);
+
+    let info = document.createElement("div");
+    info.classList.add("info");
+
+    let name = document.createElement("h1");
+    name.classList.add("name");
+    name.textContent = input[1].value;
+
+    let title = document.createElement("h3");
+    title.classList.add("title");
+    title.textContent = input[2].value;
+
+    let bioText = document.createElement("span");
+    bioText.classList.add("bio-text");
+    bioText.textContent = input[3].value;
 })
